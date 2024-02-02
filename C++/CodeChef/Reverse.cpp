bbf5b9d09cd056f229ad65ac92e5cfded27a7cf6
Nirmalya Mondal <35608135+nirmalyax@users.noreply.github.com>
2024-02-02 18:58:54 +0530

    @nirmalyax 2nd Commit

---
+++ C++/CodeChef/Reverse.cpp
@@ -0,0 +1,22 @@
+/* Write a function to reverse a string in-place. */
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+void reverseStr(string& str) {
+
+    int n = str.length();
+ 
+    // Swap character starting from two
+    // corners
+    for (int i = 0; i < n / 2; i++)
+        swap(str[i], str[n - i - 1]);
+}
+
+int main() {
+    string str = "geeksforgeeks";
+    reverseStr(str);
+    cout << str;
+    return 0;
+}
